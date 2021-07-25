cb801e3bba4f6548ddabd2300ee07297b294c7f4
Jimi Lin <chinghsiang2019@gmail.com>
2021-07-25 20:57:54 +0800

    add: lab 1

---
+++ lab1/20210724.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, world!\nwhat i'm doing is making my first lab\nisn't it cool?" << endl;
+//try to make some weird stuff
+	return 0;
+}
